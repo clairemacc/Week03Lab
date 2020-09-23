08a2b47e305e901bc2c6f2c3eb1575896a6d0bf8
822408 <822408@SAIT224224IT.ACDM.DS.SAIT.CA>
2020-09-23 11:45:49 -0600

    Added Arithmetic Calculator functionality Arithmetic Calculator now functions and validates user input.

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -26,6 +26,40 @@ public class ArithmeticCalculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
+        String firstNum = request.getParameter("firstNum");
+        String secondNum = request.getParameter("secondNum");
+        
+        int firstNumInt;
+        int secondNumInt;
+        
+        request.setAttribute("firstNum", firstNum);
+        request.setAttribute("secondNum", secondNum);
+        
+        try {
+            if (firstNum == null || firstNum.equals("") || secondNum == null || secondNum.equals("")) {
+                request.setAttribute("result", "invalid");
+            }
+            else {
+                firstNumInt = Integer.parseInt(firstNum);
+                secondNumInt = Integer.parseInt(secondNum);
+                
+                if (request.getParameter("add") != null) {
+                    request.setAttribute("result", (firstNumInt + secondNumInt));
+                }
+                if (request.getParameter("subtract") != null) {
+                    request.setAttribute("result", (firstNumInt - secondNumInt));
+                }
+                if (request.getParameter("multiply") != null) {
+                    request.setAttribute("result", (firstNumInt * secondNumInt));
+                }
+                if (request.getParameter("modulus") != null) {
+                    request.setAttribute("result", (firstNumInt % secondNumInt));
+                }
+            }
+        } catch (NumberFormatException nfe) {
+            request.setAttribute("result", "invalid");
+        }
+        
         getServletContext().getRequestDispatcher("/WEB-INF/arithmeticCalculator.jsp")
                 .forward(request, response);
     }
--- web/WEB-INF/arithmeticCalculator.jsp
+++ web/WEB-INF/arithmeticCalculator.jsp
@@ -12,7 +12,16 @@
         <title>Arithmetic Calculator</title>
     </head>
     <body>
-        <h1>Arithmetic Calculator</h1> 
-        <a href="/Week03Lab_Calculators/age">Age Calculator</a>
+        <h1>Arithmetic Calculator</h1>
+        <form method="post" action="arithmetic">
+            First: <input type="text" name="firstNum" value="${firstNum}"><br>
+            Second: <input type="text" name="secondNum" value="${secondNum}"><br>
+            <input type="submit" name="add" value="+">
+            <input type="submit" name="subtract" value="-">
+            <input type="submit" name="multiply" value="*">
+            <input type="submit" name="modulus" value="%">
+        </form><br>
+        Result: ${result}<br>
+        <a href="/Week03Lab_Calculators/age">Age Calculator</a>   
     </body>
 </html>
